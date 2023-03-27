f28b80862e1ddc7a9381e9500376dfabed4dcddc
Marte Auer <marte_auer@hotmail.com>
2023-03-27 14:37:07 +0200

    add pagination + nextpage fetch + maxpage count

--- index.js
+++ index.js
@@ -11,20 +11,40 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 
 async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
   const data = await response.json();
   
-  data.results.forEach((character) => {
-  
-    cardContainer.append(createCharacterCard(character));
+  maxPage = data.info.pages
 
+  data.results.forEach((character) => {
+    cardContainer.append(createCharacterCard(character));
   })
 }
 
 fetchCharacters();
+
+
+nextButton.addEventListener("click", () => {
+if(page < maxPage)
+  page++;
+cardContainer.innerHTML = "";
+fetchCharacters()
+pagination.innerHTML = `${page} / ${maxPage}`
+})
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    cardContainer.innerHTML = "";
+    fetchCharacters()
+    pagination.innerHTML = `${page} / ${maxPage}`
+  } 
+  
+})
+
